8ae856bab1e35e4338fdb34e397fdefbd724271e
bigbossbry <brycmramirez@gmail.com>
2022-12-01 19:07:43 +0800

    Added a range slider in html for resizing the grid. Added the necessary functions in the script file to make it work.

--- script.js
+++ script.js
@@ -1,20 +1,64 @@
 // Grid size declaration
-let gridSize = 16;
+const slider = document.querySelector('#grid-size');
+let gridSize = slider.value;
+slider.addEventListener('input', thisFunction);
+
+function thisFunction() {
+    gridSize = this.value;
+    resizeGrid();
+}
+
 
 // Grind rendering
 const container = document.querySelector('.container');
-for (let i=0; i<gridSize ** 2; i++) {
-    const squares = document.createElement('div');
-    squares.classList.add('grid-item');
-    squares.classList.add(`box-${i}`);
-    container.appendChild(squares);
+function createGrid() {
+    for (let i=0; i<gridSize ** 2; i++) {
+        const squares = document.createElement('div');
+        squares.classList.add('grid-item');
+        squares.classList.add(`box-${i}`);
+        container.appendChild(squares);
+    }
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr`;
+    let gridBlocks = document.querySelectorAll('.grid-item');
+    gridBlocks.forEach(listenHover);
 }
-container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr`;
-container.style.gridTemplateRows = `repeat(${gridSize}, 1fr`;
 
-// Main drawing function
-function hoverEvent(targetBlock) {
-    targetBlock.target.style.backgroundColor = "black";
+createGrid();
+function resizeGrid(){
+    deleteGrid();
+    createGrid();
 }
-const gridBlocks = document.querySelectorAll('.grid-item');
-gridBlocks.forEach((square) => {square.addEventListener('mouseover', hoverEvent);});
+
+//delete grid for resizing
+
+function deleteGrid() {
+    while (container.firstChild) {
+      container.removeEventListener('mouseover', shadeMe);
+      container.lastChild = null;
+      container.removeChild(container.lastChild);
+    }
+  }
+// Main drawing function
+
+function shadeMe(event) {
+   event.target.style.backgroundColor = "black";
+}
+
+function listenHover(param1) {
+    param1.addEventListener('mouseover', shadeMe);
+}
+
+// solution by Kev is addeventlistener onmouseover, execute function. inside function, check if mouse is clicked, if yes, execute function.
+
+
+
+//App algorithm
+//Create divs using Javascript. 
+//Target all JS created divs using querySelectorAll (This returns a node list)
+//Using the node list, we add event listeners to each div by using the forEach method.
+//The event listener listens for a mouseover function, then it will call the shading function.
+//The shading function must check if the mouse button is clicked, if it is, then shade the target cell
+
+
+
