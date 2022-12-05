298efa0d7bb65286a7e747ff7e5429a935080e31
bigbossbry <brycmramirez@gmail.com>
2022-12-05 14:15:30 +0800

    Created functions to make the sketchpad only draw while the mouse is clicked.

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-// Grid size declaration
+// GRID CREATION SECTION
+//Get range slider value using JS and store in a variable.
+//Pass the value to the JS function that creates the grid.
+//The grid will then be created and sized according to the value received from the range slider.
+
 const slider = document.querySelector('#grid-size');
 let gridSize = slider.value;
-slider.addEventListener('input', thisFunction);
+slider.addEventListener('input', getSliderValue);
 
-function thisFunction() {
+function getSliderValue() {
     gridSize = this.value;
     resizeGrid();
 }
 
-
-// Grind rendering
 const container = document.querySelector('.container');
 function createGrid() {
     for (let i=0; i<gridSize ** 2; i++) {
@@ -21,16 +23,12 @@ function createGrid() {
     container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr`;
     container.style.gridTemplateRows = `repeat(${gridSize}, 1fr`;
     let gridBlocks = document.querySelectorAll('.grid-item');
-    gridBlocks.forEach(listenHover);
+    gridBlocks.forEach(listenHover); 
+    gridBlocks.forEach(listenClick);
+    gridBlocks.forEach(listenMouseUp);// Add the event listeners to all the squares in the grid.
 }
 
 createGrid();
-function resizeGrid(){
-    deleteGrid();
-    createGrid();
-}
-
-//delete grid for resizing
 
 function deleteGrid() {
     while (container.firstChild) {
@@ -39,26 +37,54 @@ function deleteGrid() {
       container.removeChild(container.lastChild);
     }
   }
-// Main drawing function
 
-function shadeMe(event) {
-   event.target.style.backgroundColor = "black";
+function resizeGrid(){
+    deleteGrid();
+    createGrid();
 }
 
+// DRAWING SECTION
+// Function that checks if my mouse is clicked.
+let testValue;
+function clickFunction(e) {
+    console.log(e);
+    if (e.type == 'mousedown') {
+        return testValue = 1;
+    } else {
+        return testValue = 0;
+    }
+}
+
+function mouseUpFunction(e) {
+    console.log('mouseup');
+    return testValue = 0;
+}
+
+// Main drawing function
+function shadeMe(event) {
+    if ( testValue == 1) {
+   event.target.style.backgroundColor = "black";
+    } else {
+        return;
+    }
+}
+
+// The event listener that listens to mousehover.
 function listenHover(param1) {
     param1.addEventListener('mouseover', shadeMe);
 }
 
+// The event listener that listens to clicks.
+function listenClick(param1) {
+    param1.addEventListener('mousedown', clickFunction);
+}
+
+// The event listener that listens to mouseup.
+function listenMouseUp(param1) {
+    param1.addEventListener('mouseup', mouseUpFunction);
+}
+
 // solution by Kev is addeventlistener onmouseover, execute function. inside function, check if mouse is clicked, if yes, execute function.
-
-
-
-//App algorithm
-//Create divs using Javascript. 
-//Target all JS created divs using querySelectorAll (This returns a node list)
-//Using the node list, we add event listeners to each div by using the forEach method.
-//The event listener listens for a mouseover function, then it will call the shading function.
-//The shading function must check if the mouse button is clicked, if it is, then shade the target cell
 
 
 
